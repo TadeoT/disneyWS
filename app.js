a3b9d2ecf228f41465c3b3b3a1fc54f1541a5580
Tadeo Tiraboschi <tiraboschi.tadeo@gmail.com>
2022-02-03 10:11:18 -0300

    Remove node_modules

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/personaje', personajeRouter);
-app.use('/pelicula', peliculaRouter);
+app.use('/characters', personajeRouter);
+app.use('/movies', peliculaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/pelicula.js
+++ routes/pelicula.js
@@ -6,9 +6,20 @@ const modeloPelicula = require('../models').Pelicula
 
 router.get('/', async (req, res) => {
     let personajes = await modeloPelicula.findAll({
-        include: [{ model:modeloGenero}]
+        attributes: ['id','titulo', 'imagen'],
     })
     res.json(personajes);
+});
+router.get('/:id', async (req, res) => {
+    let personaje = await modeloPelicula.findOne({
+        where:{
+            id: req.params.id
+        },
+        include: [{ model:modeloGenero}]
+    })
+
+    if(personaje == null)  return res.status(401).json({ error: 'movie id no found' })
+    res.json(personaje);
 });
 
 module.exports = router;
